740a0fe8fe76ae2ea4d60338045ba381c5ee6eae
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2020-02-24 01:28:58 +0100

    Add making backup copy of sqlite db

--- runner.sh
+++ runner.sh
@@ -17,3 +17,5 @@ python main.py | tee $logs_dir/stats-temp.txt
 # Use single generated timestamp so it is consistent across the whole run
 utc_datetime=`sed -En 's/Current utc datetime: ([[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}T[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}Z)/\1/p' $logs_dir/stats-temp.txt`
 cp $logs_dir/stats-temp.txt $logs_dir/$utc_datetime.txt
+
+cp $save_stats_dir/'github-traffic-stats-history.sqlite' $db_backup_dir/$utc_datetime.sqlite
