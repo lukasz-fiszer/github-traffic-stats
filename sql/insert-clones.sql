c61acb4822fc6852112d3a119ef097aaf2f795ae
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2020-02-24 02:54:51 +0100

    Edit insert sql queries for clones and views
    Change queries where clause to include equality on update datetimes

--- sql/insert-clones.sql
+++ sql/insert-clones.sql
@@ -5,4 +5,4 @@ VALUES
 ON CONFLICT(full_repo_name, timestamp) DO UPDATE SET 
 	(full_repo_name, timestamp, count, uniques, utc_datetime_update) = (?, ?, ?, ?, ?)
 WHERE
-	utc_datetime_update < ?
+	utc_datetime_update <= ?
